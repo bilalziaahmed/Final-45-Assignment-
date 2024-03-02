9bf831abd12fd52fd7742bf933c0b3b4be62a359
Bilalziaahmed <bilalziaahmed@gmail.com>
2024-03-02 12:35:04 -0800

    first commit

---
+++ ASSIGNMENT 3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//STORING THE FAMOUSE CODE AND ITS AUTHOR
+let quote = "nothing is impossible beacuase impossible itsself i m posiible";
+let author = "Bilal Ahmed";
+//Printing the message
+console.log(`"${quote}" - ${author}`);
---
+++ ASSIGNMENT 3/main.ts
@@ -0,0 +1,9 @@
+//STORING THE FAMOUSE CODE AND ITS AUTHOR
+
+let quote:string = "nothing is impossible beacuase impossible itsself i m posiible"
+
+let author:string = "Bilal Ahmed";
+
+//Printing the message
+
+console.log(`"${quote}" - ${author}`);
